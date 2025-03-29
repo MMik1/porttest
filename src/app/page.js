b001a03d4dd2de72e3bb366a04dc97179e613f5f
MMik1 <144001786+MMik1@users.noreply.github.com>
2025-03-29 01:01:44 +0100

    treter
    rteter

--- src/app/page.js
+++ src/app/page.js
@@ -16,7 +16,6 @@ export default function Home() {
                 <AboutSection />
                 <ProjectsSection />
                 <EmailSection />
-                <TaalSwitchTwee />
             </div>
             <Footer />
         </main>
